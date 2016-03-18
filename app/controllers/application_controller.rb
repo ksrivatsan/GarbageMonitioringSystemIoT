1569146609f85ad9cbe3cdb7d8081465da2251b1
ksrivatsan <kauvas@gmail.com>
2016-03-18 15:42:18 -0700

    Front end changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,9 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_filter :set_all_sensors
+
+  def set_all_sensors
+  	@sensor_ids = Sensor.all.uniq.pluck(:sensor_id)
+  end
 end
